367cd489c577b78c302053da1016bd7d95bb6b82
tung <tung@DESKTOP-2C2OF6D>
2018-06-05 10:33:53 +0700

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbkichthuoc = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbbkichthuoc
+            // 
+            this.cbbkichthuoc.FormattingEnabled = true;
+            this.cbbkichthuoc.Items.AddRange(new object[] {
+            "3 x 3",
+            "4 x 4",
+            "5 x 5",
+            "6 x 6",
+            "7 x 7",
+            "8 x 8"});
+            this.cbbkichthuoc.Location = new System.Drawing.Point(82, 12);
+            this.cbbkichthuoc.Name = "cbbkichthuoc";
+            this.cbbkichthuoc.Size = new System.Drawing.Size(106, 28);
+            this.cbbkichthuoc.TabIndex = 0;
+            this.cbbkichthuoc.SelectedIndexChanged += new System.EventHandler(this.cbbkichthuoc_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(343, 411);
+            this.Controls.Add(this.cbbkichthuoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbkichthuoc;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        void createbutton(string n, int a, int b1, int c, int d)
+        {
+            Button b = new Button();
+            b.Name = n;
+            b.Size = new Size(a, b1);
+            b.Location = new Point(c, d);
+            
+            this.Controls.Add(b);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+
+
+
+        }
+
+        void drawscreen()
+        {
+
+            int t = cbbkichthuoc.SelectedIndex;
+            int n=0;
+            switch(t)
+            {
+                case 0:
+                    n = 3;
+                    break;
+                case 1:
+                    n = 4;
+                    break;
+                case 2:
+                    n = 5;
+                    break;
+                case 3:
+                    n = 6;
+                    break;
+                case 4:
+                    n = 7;
+                    break;
+                case 5:
+                    n = 8;
+                    break;
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    string name = "bt" + i.ToString() + j.ToString();
+                    createbutton(name, 40, 40, i * 40+2, j * 40+40);
+                }
+            }
+            this.Size = new Size(n * 40 + 2, n * 40 + 40);
+        }
+
+
+      
+        private void cbbkichthuoc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            foreach (Control item  in this.Controls)
+             {
+                if (item is Button)
+                    this.Controls.Remove(item);
+            }
+           // this.Controls.Clear();
+            drawscreen();
+           
+        }
+    }
+}
